31432d1e7ae5252a6630063aa01d46d704d7de1a
Mihai <mihai.muscalu@student.htldornbirn.at>
2023-12-05 19:01:22 +0100

    script and api

---
+++ Figma/script.js
@@ -0,0 +1,33 @@
+const loadData = () => {
+
+    fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result) => {
+        result.json().then((data) => {
+            fillTable(data);
+        })
+    })
+}
+
+
+
+const fillTable = (data) => {
+    let Icon = ""
+    let Teams = "";
+    let Matches = "";
+    let Won = "";
+    let GD = "";
+    let Points = "";
+    for (let i = 0; i < 9; i++) {
+        Icon = "<td><img class='Icon' src='" + data[i].teamIconUrl + "'></img></td>";
+        Teams = "<td class='Teams'>" + data[i].teamName + "</td>";
+        Matches = "<td class='Matches'>" + data[i].matches + "</td>";
+        Won = "<td class='Won'>" + data[i].won + "</td>";
+        GD = "<td class='GD'>" + data[i].goalDiff + "</td>";
+        Points = "<td class='Points'>" + data[i].points + "</td>";
+        document.getElementById("row").innerHTML += "<tr>" + Icon + Teams + Matches + Won + GD + Points + "</td>";
+
+    };
+
+
+}
+
+loadData();
